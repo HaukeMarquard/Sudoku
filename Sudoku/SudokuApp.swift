fb03629d73a5a4a1a62d4ecf08abe8f8171de67f
Hauke Marquard <Hauke@marquard.tech>
2023-01-28 18:58:46 +0100

    Initial Commit

---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Hauke Marquard on 28.01.23.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
